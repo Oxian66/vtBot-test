ada17272e25a79bbde312c964b208df875a5a406
Tamara <Oxian66@mail.ru>
2024-09-24 16:48:28 +0300

    comment useEffect

--- src/App.js
+++ src/App.js
@@ -5,32 +5,32 @@ import VisionTestApp from "./components/visionTestApp/VisionTestApp";
 
 const App = () => {
   // Получение объекта WebApp API
-  const TelegramWebApp = window.Telegram.WebApp;
+  // const TelegramWebApp = window.Telegram.WebApp;
 
-  // Используем useEffect для инициализации WebApp
-  useEffect(() => {
-    // Сообщаем Telegram, что WebApp готово к работе
-    TelegramWebApp.ready();
+  // // Используем useEffect для инициализации WebApp
+  // useEffect(() => {
+  //   // Сообщаем Telegram, что WebApp готово к работе
+  //   TelegramWebApp.ready();
 
-    // Опционально: можно настроить цвет заголовка
-    TelegramWebApp.MainButton.setText("Закрыть приложение");
-    TelegramWebApp.MainButton.show();
+  //   // Опционально: можно настроить цвет заголовка
+  //   TelegramWebApp.MainButton.setText("Закрыть приложение");
+  //   TelegramWebApp.MainButton.show();
 
-    // Можно получить информацию о пользователе
-    console.log('User data:', TelegramWebApp.initDataUnsafe);
+  //   // Можно получить информацию о пользователе
+  //   console.log('User data:', TelegramWebApp.initDataUnsafe);
 
-    // Опционально: Закрытие приложения при нажатии на кнопку
-    TelegramWebApp.MainButton.onClick(() => {
-      TelegramWebApp.close();
-    });
+  //   // Опционально: Закрытие приложения при нажатии на кнопку
+  //   TelegramWebApp.MainButton.onClick(() => {
+  //     TelegramWebApp.close();
+  //   });
 
-    return () => {
-      // Удаляем обработчик события при размонтировании компонента
-      TelegramWebApp.MainButton.offClick(() => {
-        TelegramWebApp.close();
-      });
-    };
-  }, [TelegramWebApp]);
+  //   return () => {
+  //     // Удаляем обработчик события при размонтировании компонента
+  //     TelegramWebApp.MainButton.offClick(() => {
+  //       TelegramWebApp.close();
+  //     });
+  //   };
+  // }, [TelegramWebApp]);
   return (
     // Returning main app component
     <Fragment>
